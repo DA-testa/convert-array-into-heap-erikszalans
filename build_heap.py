b32f104aff29af2345e0967bfc339439285245c5
erikszalans <125026714+erikszalans@users.noreply.github.com>
2023-03-17 20:07:36 +0000

    commit

--- build_heap.py
+++ build_heap.py
@@ -3,25 +3,59 @@
 
 def build_heap(data):
     swaps = []
+    garums = len(data)
+    for i in range(garums//2, -1,-1):
+         swaps = heap(data,i,swaps)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
+    return swaps
 
+def heap(data, i ,swaps):
+    garums = len(data)
+    while i*2+1 < garums:
+        j = i*2+1
+        if j+1 < garums and data[j+1] < data[j]:
+            j += 1
+        if data[i] <= data[j]:
+            break
+        data[i], data[j], = data[j], data[i]
+        swaps.append((i,j))  
+        i=j
+
+    return swaps
+
+def heap_sort(data):
+    swaps = build_heap(data)
+    garums = len(data)
+    for i in range(garums-1, 0, -1):
+        data[0], data[i] = data[i],data[0]
+        swaps.append((0,i))
+        swaps = heap(data, 0, swaps[:])
 
     return swaps
 
 
 def main():
     
-    # TODO : add input and corresponding checks
+            # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    ievade = input()
+    if "I" in ievade:
+        n = int(input())
+        data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
+    elif "F" in ievade:
+
+        filename = input()
+        with open(f"tests/{filename}") as f:
+            n = int(f.readline())
+            data = list(map(int, input()))
+
+
+
     assert len(data) == n
 
     # calls function to assess the data 
@@ -36,6 +70,7 @@ def main():
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+        print(len(swaps))
 
 
 if __name__ == "__main__":
